c7fbfdbd6fb3ff734a81a9cedb4c9b8669642033
Duy ND <duynd1006@gmail.com>
2025-03-11 21:14:26 +0000

    create listing layout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -14,6 +14,7 @@ import {
 } from "../store/user/userSlice"
 import { useDispatch } from "react-redux"
 import { app } from "../firebase"
+import { Link } from "react-router-dom"
 
 export default function Profile() {
   const dispatch = useDispatch()
@@ -203,6 +204,12 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          Create listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
