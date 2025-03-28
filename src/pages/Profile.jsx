7e3220b35fc50e4a4ff0b48214bf55f182eb7e74
Duy ND <duynd1006@gmail.com>
2025-03-28 18:03:43 +0000

    listing delete

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -25,6 +25,8 @@ export default function Profile() {
   const [filePercentage, setFilePercentage] = useState(0)
   const [fileUploadError, setFileUploadError] = useState(false)
   const [updateSuccess, setUpdateSuccess] = useState(false)
+  const [listings, setListing] = useState([])
+  const [showListingError, setShowListingError] = useState(false)
 
   useEffect(() => {
     if (file) {
@@ -132,6 +134,41 @@ export default function Profile() {
     }
   }
 
+  const handleShowListings = async () => {
+    try {
+      setShowListingError(false)
+      const res = await fetch(`/api/user/listings/${currentUser._id}`)
+      const data = await res.json()
+
+      if (data.success === false) {
+        setShowListingError(true)
+        return
+      }
+
+      setListing(data)
+    } catch (error) {
+      setShowListingError(true)
+    }
+  }
+
+  const handleDeleteListing = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE"
+      })
+
+      const data = await res.json()
+      if (data.success === false) {
+        console.log(data.message)
+        return
+      }
+
+      setListing((prev) => prev.filter((listing) => listing._id !== listingId))
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -226,6 +263,57 @@ export default function Profile() {
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">{updateSuccess ? "Updated Successfully!" : ""}</p>
+      <button
+        onClick={handleShowListings}
+        className="text-green-700 w-full"
+      >
+        Show Listings
+      </button>
+      <p className="text-green-700 mt-5">
+        {showListingError ? "Error showing listings!" : ""}
+      </p>
+      {
+        listings && listings.length > 0 &&
+        <div className="flex flex-col gap-4">
+          <h1
+            className="text-center mt-7 text-2xl font-semibold"
+          >
+            Your Listings
+          </h1>
+          {
+            listings.map((listing) => (
+              <div
+                className="border rounded-lg p-3 flex justify-between items-center gap-4"
+                key={listing._id}
+              >
+                <Link to={`/listing/${listing._id}`}>
+                  <img
+                    src={listing.imageUrls[0]} alt="listing cover"
+                    className="h-16 w-16 object-contain"
+                  />
+                </Link>
+                <Link
+                  className="flex-1 text-slate-700 font-semibold hover:underline truncate"
+                  to={`/listing/${listing._id}`}
+                >
+                  <p>{listing.name}</p>
+                </Link>
+                <div className="flex flex-col items-center">
+                  <button
+                    onClick={() => handleDeleteListing(listing._id)}
+                    className="text-red-700 uppercase"
+                  >
+                    Delete
+                  </button>
+                  <button className="text-green-700 uppercase">
+                    Edit
+                  </button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      }
     </div>
   )
 }
