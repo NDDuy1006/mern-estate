85087e311419a4a0c82521994ab5a8f6914472f6
Duy ND <duynd1006@gmail.com>
2025-04-01 21:18:25 +0100

    search page filter

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,42 @@
+import { useState } from "react"
 
 
 export default function Search() {
+  const [sidebarData, setSidebarData] = useState({
+    searchTerm: "",
+    type: "all",
+    parking: false,
+    furnished: false,
+    offer: false,
+    sort: "created_at",
+    order: "desc"
+  })
+
+  const handleChange = (e) => {
+    if (e.target.id === "all" || e.target.id === "rent" || e.target.id === "sell") {
+      setSidebarData({ ...sidebarData, type: e.target.id })
+    }
+
+    if (e.target.id === "searchTerm") {
+      setSidebarData({ ...sidebarData, searchTerm: e.target.value })
+    }
+
+    if (
+      e.target.id === "parking"
+      || e.target.id === "furnished"
+      || e.target.id === "offer"
+    ) {
+      setSidebarData({ ...sidebarData, [e.target.id]: e.target.checked || e.target.checked === "true" ? true : false })
+    }
+
+    if (e.target.id === "sort_order") {
+      const sort = e.target.value.split("_")[0] || "created_at"
+      const order = e.target.value.split("_")[1] || "desc"
+
+      setSidebarData({ ...sidebarData, type: e.target.id })
+    }
+  }
+
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
@@ -12,48 +48,96 @@ export default function Search() {
               id="searchTerm"
               placeholder="Search..."
               className="border rounded-lg p-3 w-full"
+              value={sidebarData.searchTerm}
+              onChange={handleChange}
             />
           </div>
           <div className="flex gap-2 flex-wrap items-center">
             <label className="font-semibold">Type:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="all" className="w-5" />
-              <span>Rent & Sale</span>
+              <input
+                type="checkbox"
+                id="all"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.type === "all"}
+              />
+              <span>Rent & Sell</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="rent" className="w-5" />
+              <input
+                type="checkbox"
+                id="rent"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.type === "rent"}
+              />
               <span>Rent</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="sale" className="w-5" />
-              <span>Sale</span>
+              <input
+                type="checkbox"
+                id="sell"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.type === "sell"}
+              />
+              <span>Sell</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="offer" className="w-5" />
+              <input
+                type="checkbox"
+                id="offer"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.offer}
+              />
               <span>Offer</span>
             </div>
           </div>
           <div className="flex gap-2 flex-wrap items-center">
             <label className="font-semibold">Amenities:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="parking" className="w-5" />
+              <input
+                type="checkbox"
+                id="parking"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.parking}
+              />
               <span>Parking</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="furnished" className="w-5" />
+              <input
+                type="checkbox"
+                id="furnished"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebarData.furnished}
+              />
               <span>Furnished</span>
             </div>
           </div>
           <div className="flex items-center gap-2">
             <label className="font-semibold">Sort</label>
             <select
+              onChange={handleChange}
+              defaultValue={"created_at_desc"}
               id="sort_order"
               className="border rounded-lg p-3"
             >
-              <option>Price hight to low</option>
-              <option>Price low to high</option>
-              <option>Latest</option>
-              <option>Oldest</option>
+              <option value="regularPrice-desc">
+                Price hight to low
+              </option>
+              <option value="regularPrice-asc">
+                Price low to high
+              </option>
+              <option value="createdAt_desc">
+                Latest
+              </option>
+              <option value="createdAt_asc">
+                Oldest
+              </option>
             </select>
           </div>
           <button
